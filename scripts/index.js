6d3af5e71d8c734860cebe34e9b484b7c7ebf74b
Alex Tcoi <alexvtcoi@gmail.com>
2020-12-08 20:20:12 +0100

    доработка после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.form__close-button');
-const form = document.querySelector('.form');
-const name = document.getElementById('name');
-const profession = document.getElementById('profession');
-const formName = document.getElementById('form-name');
-const formProfession = document.getElementById('form-profession');
-const formContainer = document.querySelector('.form__container');
-const cardForm = document.querySelector('.form_card');
+const form = document.querySelector('.popup_profile');
+const name = document.querySelector('#name');
+const profession = document.querySelector('#profession');
+const formName = document.querySelector('#form-name');
+const formProfession = document.querySelector('#form-profession');
+const formContainer = document.querySelector('.form');
+const cardForm = document.querySelector('.popup_card');
 const cardFormClose = document.querySelector('[name="card_close"]');
 const cardFormOpen = document.querySelector('.profile__add-button');
 const cardFormContainer = document.querySelector('[name="card_form"]');
@@ -15,8 +15,11 @@ const cardTemplate = document.querySelector('#card_template');
 const inputCardName = document.querySelector('[name="card_name"]');
 const inputCardLink = document.querySelector('[name="card_link"]');
 const saveNewCardButton = document.querySelector('[name="card_save"]');
-const openedCard = document.querySelector('.opened-card');
+const openedCard = document.querySelector('.popup_opened-card');
 const openedCardClose = document.querySelector('.opened-card__close-button');
+const openedCardName = openedCard.querySelector('.opened-card__name');
+const openedCardPic = openedCard.querySelector('.opened-card__pic');
+
 const initialCards = [
     {
         name: 'Архыз',
@@ -76,20 +79,27 @@ function composeCard({ name, link }){
 }//формирование карточки + работа лайка + удаление карточки + открытие вложенной картинки
 
 
-function openCardPicture ({ name, link }) {
-    const openedCardName = openedCard.querySelector('.opened-card__name');
-    const openedCardPic = openedCard.querySelector('.opened-card__pic');
+function openCardPicture ({ name, link }) {  
     openedCardName.textContent = name;
     openedCardPic.src = link;
     openedCardPic.alt = name;
-    openedCard.classList.add('opened-card_opened');
+    openPopup(openedCard);
 }//открытие картинки
 
 
 openedCardClose.addEventListener('click', function(evt) {
-    const targetPicture = evt.target.closest('.opened-card');
-    targetPicture.classList.remove('opened-card_opened');  
+    const targetPicture = evt.target.closest('.popup');
+    closePopup(targetPicture);  
 });//закрытие картинки
+
+
+function openPopup(popup) {
+    popup.classList.add('popup_opened');
+} //открытие попапов
+
+function closePopup(popup) {
+    popup.classList.remove('popup_opened');
+} //закрытие попапов
 
 
 function addNewCard (evt){
@@ -101,45 +111,34 @@ function addNewCard (evt){
     closeCardForm();
 }//создание новой карточки
 
-saveNewCardButton.addEventListener('click', addNewCard);//триггер для сохранения новой карточки
-
-
-function openCardForm () {
-    cardForm.classList.add('form_opened');
-}//открытие попапа для карточки
-cardFormOpen.addEventListener('click', openCardForm);//триггер открытия попапа для карточки
-
-
-function closeCardForm() {
-    cardForm.classList.remove('form_opened');
-    cardFormContainer.reset();
-}//закрытие попапа для карточки
-cardFormClose.addEventListener('click', closeCardForm);//триггер закрытия попапа для карточки
-
-
 function openForm() {
     formName.setAttribute('value', name.textContent);
     formProfession.setAttribute('value', profession.textContent);
-    form.classList.add('form_opened');
+    openPopup(form);
 }//открытие попапа для профиля
-editButton.addEventListener('click', openForm);//триггер открытия попапа для профиля
 
+function closeCardForm() {
+    closePopup(cardForm);
+    cardFormContainer.reset()
+}//закрытие попапа формы карточки
 
 function closeForm() {
-    form.classList.remove('form_opened');
-    formContainer.reset();
-}//закрытие попапа для профиля
-closeButton.addEventListener('click', closeForm);//триггер закрытия попапа для профиля
-
+    closePopup(form);
+    formContainer.reset()
+}//закрытие попапа формы профиля
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    let nameValue = document.getElementById('form-name').value;
-    let professionValue = document.getElementById('form-profession').value;
+    let nameValue = formName.value;
+    let professionValue = formProfession.value;
     name.textContent = nameValue;
     profession.textContent = professionValue;
-    closeForm();
-    formName.setAttribute('value', name.textContent);
-    formProfession.setAttribute('value', profession.textContent);
+    closePopup(form);
 }//сохранение имени и профессии из попапа профиля
+
+saveNewCardButton.addEventListener('click', addNewCard);//триггер для сохранения новой карточки
+cardFormOpen.addEventListener('click', ()=>openPopup(cardForm));//триггер открытия попапа для карточки
+cardFormClose.addEventListener('click', closeCardForm);//триггер закрытия попапа для карточки
+editButton.addEventListener('click', openForm);//триггер открытия попапа для профиля
+closeButton.addEventListener('click', closeForm);//триггер закрытия попапа для профиля
 formContainer.addEventListener('submit', formSubmitHandler);//триггер сохранения имени и профессии из попапа профиля
